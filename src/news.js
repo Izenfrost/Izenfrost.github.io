ca239295cf9e64ff84bef26980bf8bbf24cc60ed
Izenfrost <izenfrost@gmail.com>
2018-03-05 12:02:05 +0300

    Add structural pattern: facade

--- src/news.js
+++ src/news.js
@@ -1,26 +1,49 @@
 import '../style/style.css'
 
-var show_news = document.createElement("button");
-show_news.textContent = "Show News";
-document.body.appendChild(show_news);
-
-show_news.addEventListener("click", function () {
-    import('./lib/create_news').then(create_news => {
-        if (process.env.NODE_ENV !== 'production') {
-            console.log('Development mode');
-        }
-
-        var url = 'https://newsapi.org/v2/top-headlines?' +
+const url = 'https://newsapi.org/v2/top-headlines?' +
             'country=ru&' +
             'category=technology&' +
             'apiKey=94d80f877d70435eafbc7f57e02da2a5';
 
-        var request = new Request(url);
+let News = function(url) {
+    this.url = url
+}
 
+News.prototype = {
+    createShowButton: function() {
+        let that = this;
+        let show_news = document.createElement("button");
+
+        show_news.textContent = "Show News";
+        document.body.appendChild(show_news);
+
+        show_news.addEventListener("click", function () {
+            import('./lib/create_news').then(create_news => {
+                if (process.env.NODE_ENV !== 'production') {
+                    console.log('Development mode');
+                }
+
+                that.createRequest(create_news);
+            });
+        });
+    },
+
+    createRequest: function(create_news) {
+        let request = new Request(this.url);
+        
         fetch(request)
             .then((response) => response.json())
             .then(function (feed) {
                 feed.articles.filter((article) => article.description != null).map(create_news.createNews);
             });
-    });
-});
+    }
+}
+
+function run() {
+    let news = new News(url);
+    news.createShowButton();
+};
+
+run();
+
+
